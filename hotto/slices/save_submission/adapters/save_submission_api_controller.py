e64d6c993a1e477ed966e493a4a937b6cf3554eb
David Vega <dukeofgaming@gmail.com>
2025-04-30 01:31:13 -0500

    Renamed usecase layer to application layer

--- hotto/slices/save_submission/adapters/save_submission_api_controller.py
+++ hotto/slices/save_submission/adapters/save_submission_api_controller.py
@@ -3,20 +3,21 @@ from mysql.connector import Error
 from flask import jsonify, current_app
 from hotto.modules.survey.domain.entities.submission import Submission
 from hotto.modules.survey.infrastructure.repositories.mysql_submission_repository import MySQLSubmissionRepository
-from hotto.slices.save_submission.usecases.save_submission_usecase import SaveSubmissionUseCase
+from hotto.slices.save_submission.application.save_submission_usecase import SaveSubmissionUseCase
 
 class SaveSubmissionApiController:
+    def __init__(self):
+        submission_repository = MySQLSubmissionRepository()
+        self.use_case = SaveSubmissionUseCase(submission_repository)
+
     def save_submission(self, request):
         try:
-            # The repository will handle DB config and connection internally
-            submission_repository = MySQLSubmissionRepository()
-            use_case = SaveSubmissionUseCase(submission_repository)
             data = request.get_json()
             if not data:
                 return {"error": "Invalid or missing JSON payload."}, 400
 
             submission = Submission.from_dict(data)
-            response, status_code = use_case.save_submission(submission, data['answers'])
+            response, status_code = self.use_case.save_submission(submission, data['answers'])
             return response, status_code
         except Error as err:
             return {"error": str(err)}, 500
